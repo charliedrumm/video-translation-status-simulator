50320e05bdbb418288ed62da270b4e2720641b2c
charliedrumm <charlie.drumm@ucdconnect.ie>
2024-12-10 17:05:18 +0000

    run each task on thread

--- client_test.py
+++ client_test.py
@@ -1,37 +1,27 @@
 from client.CheckStatus import CheckStatus
-import time
+from concurrent.futures import ThreadPoolExecutor
 
 
 def test_client_library():
-    # Server configuration
     server_url = "http://localhost:5000"
     websocket_url = "http://localhost:5000/socket.io/"
-
-    # Initialize the client library
     client = CheckStatus(server_url, websocket_url)
-
-    # Connect to the WebSocket server
     client.connect()
 
     try:
-        # Create tasks
-        task_ids = [client.create_task() for _ in range(3)]
-        print(f"Created tasks: {task_ids}")
+        #Threads run this function
+        def create_and_wait_task(delay):
+            task_id, result = client.create_and_wait(delay)
+            print(f"Task {task_id} completed with status: {result['status']}")
 
-        # Wait for each task to complete
-        for task_id in task_ids:
-            status = client.wait_for_task(task_id)
-            print(f"Task {task_id} completed with status: {status}")
-        
-        task_id = client.create_task()
-        time.sleep(10)
-        print(client.get_status(task_id))
-        print(client.wait_for_task(task_id))
-
+        #I am using ThreadPoolExecuter to simulate multiple tasks being made at once
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            task_times = [10,4,6,7,8] #make the tasks different times to show shorter tasks are not blocked
+            for i in range(5):
+                executor.submit(create_and_wait_task, task_times[i])
+            
     finally:
-        # Disconnect from the WebSocket server
         client.disconnect()
-
 
 if __name__ == "__main__":
     test_client_library()
